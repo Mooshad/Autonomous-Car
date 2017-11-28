39ac7de4c7c47e206074c208b52128d92071f080
lijiefufu <33871758+lijiefufu@users.noreply.github.com>
2017-11-28 09:57:58 +0800

    Update CGSolver.py

--- CGSolver.py
+++ CGSolver.py
@@ -1,31 +1,120 @@
 from car import car
+from copy import deepcopy
+from random import *
+def carInterfere(mycar,othercars):
 
-def CrashDetect(mycar, othercars,t,distancethresh):
-    numberOfCarsMayCrash = 0
-    myposition = [0, 0]
-    otherposition = [0, 0]
-    myposition[0] = mycar.position[0] + t * car.vel[0]
-    myposition[1] = mycar.position[1] + t * car.vel[1]
+    numberOfInterfere = 0
+
+    mylane = mycar.lane
+
+    lane0 = [0,1,2,4,5,6,8,11]
+    lane1 = [0,1,2,5,7,8,9,11]
+    lane2 = [0,1,2,3,4,5,6,7,8,9,10,11]
+    lane3 = [2,3,4,5,7,8,9,11]
+    lane4 = [0,2,3,4,5,8,10,11]
+    lane5 = [0,1,2,3,4,5,6,7,8,9,10,11]
+    lane6 = [0,2,5,6,7,8,10,11]
+    lane7 = [1,2,3,5,6,7,8,11]
+    lane8 = [0,1,2,3,4,5,6,7,8,9,10,11]
+    lane9 = [1,2,3,5,8,9,10,11]
+    lane10 = [2,4,5,6,8,9,10,11]
+    lane11 = [0,1,2,3,4,5,6,7,8,9,10,11]
+
     for c in othercars:
-        otherposition[0] = c.position[0] + t * c.vel[0]
-        otherposition[1] = c.position[1] + t * c.vel[1]
-        dist = ((myposition[0] - otherposition[0]) ** 2 + (myposition[1] - otherposition[1]) ** 2) ** 0.5
-        if dist >= distancethresh: numberOfCarsMayCrash += 1
-    return numberOfCarsMayCrash
 
-def Payoff(p, crashPenalty, mycar, othercars, t, distancethresh):
-    n = CrashDetect(mycar, othercars, t, distancethresh)
-    if len(othercars) != 0:
-        return mycar.tt
+        otherlane = c.lane
+
+        if mylane == 0 and lane0.count(otherlane)== 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 1 and lane1.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 2 and lane2.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 3 and lane3.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 4 and lane4.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 5 and lane5.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 6 and lane6.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 7 and lane7.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 8 and lane8.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 9 and lane9.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 10 and lane10.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+        if mylane == 11 and lane11.count(otherlane) == 0:
+            numberOfInterfere = numberOfInterfere + 1
+
+    return numberOfInterfere
+
+def Payoff(p,crashPenalty,mycar,othercars):
+    n = carInterfere(mycar,othercars)
+    if n == 0:
+        return 0
     else:
-        return mycar.tt + crashPenalty * p * (n-1) / (len(othercars) - n-1)
+        return crashPenalty * p * n/ (len(othercars) - n-1)
 
-def MergeAndSplit(p, crashPenalty,carLists,t,distancethresh):
-    GtoAcc = []
-    GtoAcc = []
+def MergeAndSplit(p, crashPenalty,carLists,maxIteration):
+    CoalitiontoGo = []
+    CoalitionNottoGo = []
+    # init coalition in a trivial way.
+    for c in carLists:
+        if Payoff(p,crashPenalty,c,carLists) == 0:
+            CoalitiontoGo.append(c)
+        else:
+            CoalitionNottoGo.append(c)
 
-    for carind in range(0,len(carLists)):
-        mycar = carLists[carind]
-        payoffvector[carind] = Payoff(p,crashPenalty,mycar,carLists,t,distancethresh)
+    currentPayoff = 0
+    # init best group payoff with init coalition.
 
-    return 0
+    for cinc in CoalitiontoGo:
+        currentPayoff = currentPayoff + Payoff(p,crashPenalty,cinc,CoalitiontoGo)
+    currentPayoff = currentPayoff + len(CoalitionNottoGo)
+
+    bestPayoff = deepcopy(currentPayoff)
+    answer_InCoalition = deepcopy(CoalitiontoGo)
+    answer_OutCoalition = deepcopy(CoalitionNottoGo)
+
+    if len(CoalitionNottoGo) == 0:
+        return answer_InCoalition,answer_OutCoalition
+    else:
+        # for merge
+        for m in range(maxIteration):
+            # random pick a car to join the coalition.
+            ind = randint(0,len(CoalitionNottoGo)-1)
+            cartomerge = CoalitionNottoGo[ind]    # potential bug !!!
+            CoalitiontoGo.append(cartomerge)
+            CoalitiontoGo.remove(cartomerge)
+
+            currentPayoff = 0
+            for cinc in CoalitiontoGo:
+                currentPayoff = currentPayoff + Payoff(p, crashPenalty, cinc, CoalitiontoGo)
+            currentPayoff = currentPayoff + len(CoalitionNottoGo)
+
+            if bestPayoff >= currentPayoff:
+                answer_InCoalition = deepcopy(CoalitiontoGo)
+                answer_OutCoalition = deepcopy(CoalitionNottoGo)
+                bestPayoff = currentPayoff
+        # for split
+        for m in range(maxIteration):
+            # random pick a car to split from the coalition.
+            ind = randint(0,len(CoalitiontoGo)-1)
+            cartomerge = CoalitionNottoGo[ind]    # potential bug !!!
+            CoalitiontoGo.remove(cartomerge)
+            CoalitiontoGo.append(cartomerge)
+
+            currentPayoff = 0
+            for cinc in CoalitiontoGo:
+                currentPayoff = currentPayoff + Payoff(p, crashPenalty, cinc, CoalitiontoGo)
+            currentPayoff = currentPayoff + len(CoalitionNottoGo)
+
+            if bestPayoff >= currentPayoff:
+                answer_InCoalition = deepcopy(CoalitiontoGo)
+                answer_OutCoalition = deepcopy(CoalitionNottoGo)
+                bestPayoff = currentPayoff
+
+    return answer_InCoalition,answer_OutCoalition
