441852afb87d0f2983ebe3684453b15489732dcf
Jake Papineau <jakep622@gmail.com>
2017-11-28 06:25:27 -0500

    I AM A MAN OF MY WORD

--- car.py
+++ car.py
@@ -1,97 +1,280 @@
 from random import *
+from scipy import interpolate
+import numpy as np
 import math
+
 
 class car:
     # Initialize car.
     # lane is a lane in the world
     # vel is a 2d vector containing current x and y velocity.
     # acc is a 2d vector containing current x and y acceleration.
-    def __init__(self, lane, acc, color):
+    def __init__(self, lane, color):
         self.position = lane.scp
         self.lane = lane
         self.lane_index = lane.index
-        self.acc = [0,0]
+        self.acc = 0
         self.color = color
         self.time = 0
+        self.vel = randint(3,5)
+        # Records the last position the car was at,
+        # is used to capture the heading for displaying
+        self.last_position = None
 
-        # rand_vel is the velocity
-        rand_vel = randint(3,5)
-
-        # Set the velocity based off of lane index
-        if lane.index >= 0 and lane.index <= 2:
-            self.vel = [rand_vel, 0]
-        elif lane.index > 2 and lane.index <= 5:
-            self.vel = [0, rand_vel]
-        elif lane.index > 5 and lane.index <= 8:
-            self.vel = [-rand_vel, 0]
-        elif lane.index > 8 and lane.index <= 11:
-            self.vel = [0, -rand_vel]
+        # Set initial car rotation
+        if (lane.index > 2 and lane.index <= 5) or (lane.index > 8 and lane.index <= 11):
+            self.rotation = 90.0
+        else:
+            self.rotation = 0.0
         
     # On tick, update velocity and acceleration
     def on_tick(self, new_acc):
 
-        self.vel[0] = self.vel[0] + self.acc[0]
-        self.vel[1] = self.vel[1] + self.acc[1]
+        self.last_position = self.position
+        self.vel = self.vel + self.acc
 
-        # Handle case when not in the intersection
-        if out_of_intersection(self):
-            self.position[0] = self.position[0] + self.vel[0]
-            self.position[1] = self.position[1] + self.vel[1]
-        
-        # Handle case when in the intersection
+        lane = self.lane
+        # Handle case when car hasn't entered the intersection yet
+        if before_intersection(self):
+            
+            if lane.index >= 0 and lane.index <= 2:
+                self.position[0] = self.position[0] + self.vel
+            elif lane.index > 2 and lane.index <= 5:
+                self.position[1] = self.position[1] + self.vel
+            elif lane.index > 5 and lane.index <= 8:
+                self.position[0] = self.position[0] - self.vel
+            elif lane.index > 8 and lane.index <= 11:
+                self.position[1] = self.position[1] - self.vel
+
+        # Handle case when car has already made it through the intersection
+        # Also handles car rotation for after the intersection
+        elif after_intersection(self):
+            if lane.index == 11 or lane.index == 7 or lane.index == 3:
+                self.position[0] = self.position[0] - self.vel
+                self.rotation = 0.0
+            elif lane.index == 2 or lane.index == 10 or lane.index == 6:
+                self.position[1] = self.position[1] - self.vel
+                self.rotation = 90.0
+            elif lane.index == 9 or lane.index == 1 or lane.index == 5:
+                self.position[0] = self.position[0] + self.vel
+                self.rotation = 0.0
+            elif lane.index == 0 or lane.index == 4 or lane.index == 8:
+                self.position[1] = self.position[1] + self.vel
+                self.rotation = 90.0
+                
+        # Handle traveling direction when in the intersection through
+        # interpolation. Also find new car rotation based off direction
+        # vector.
         else:
-            dx = self.position[0] - self.lane.eip[0]
-            dy = self.position[1] - self.lane.eip[1]
-            dz = math.sqrt(dx**2 + dy**2)
-
-            self.vel[0] = int(dx/dz * self.vel[0])
-            self.vel[1] = int(dy/dz * self.vel[1])
-
-            self.position[0] = self.position[0] + self.vel[0]
-            self.position[1] = self.position[1] + self.vel[1]
-
+            self.position = intersection_pos(self)
+        
         self.acc = new_acc
         self.time = self.time + 1
-            
 
             
 def check_collision(car1, car2):
     pass
 
-    
-def out_of_intersection(car):
+
+# Check if car hasn't entered the intersection yet    
+def before_intersection(car):
     lane = car.lane
     l_index = lane.index
     pos = car.position
 
     # Left lanes
     if l_index == 0:
-        return pos[0] < lane.sip[0] or pos[1] > lane.eip[1]
+        return pos[0] < lane.sip[0]
     elif l_index == 1:
-        return pos[0] < lane.sip[0] or pos[0] > lane.eip[0]
+        return pos[0] < lane.sip[0]
     elif l_index == 2:
-        return pos[0] < lane.sip[0] or pos[1] < lane.eip[1]
-
+        return pos[0] < lane.sip[0]
+    
     # Bottom lanes
     elif l_index == 3:
-        return pos[1] < lane.sip[1] or pos[0] < lane.eip[0]
+        return pos[1] < lane.sip[1]
     elif l_index == 4:
-        return pos[1] < lane.sip[1] or pos[1] > lane.eip[1]
+        return pos[1] < lane.sip[1]
     elif l_index == 5:
-        return pos[1] < lane.sip[1] or pos[0] > lane.eip[0]
+        return pos[1] < lane.sip[1]
 
     # Right lanes
     elif l_index == 6:
-        return pos[0] > lane.sip[0] or pos[1] < lane.eip[1]
+        return pos[0] > lane.sip[0]
     elif l_index == 7:
-        return pos[0] > lane.sip[0] or pos[0] < lane.eip[0]
+        return pos[0] > lane.sip[0]
     elif l_index == 8:
-        return pos[0] > lane.sip[0] or pos[1] > lane.eip[1]
+        return pos[0] > lane.sip[0]
 
     # Top lanes
     elif l_index == 9:
-        return pos[1] > lane.sip[1] or pos[0] > lane.eip[0]
+        return pos[1] > lane.sip[1]
     elif l_index == 10:
-        return pos[1] > lane.sip[1] or pos[1] < lane.eip[1]
+        return pos[1] > lane.sip[1]
     elif l_index == 11:
-        return pos[1] > lane.sip[1] or pos[0] < lane.eip[0]
+        return pos[1] > lane.sip[1]
+
+
+# Check if car has made it through the intersection
+def after_intersection(car):
+    lane = car.lane
+    l_index = lane.index
+    pos = car.position
+
+    # Left lanes
+    if l_index == 0:
+        return pos[1] > lane.eip[1]
+    elif l_index == 1:
+        return pos[0] > lane.eip[0]
+    elif l_index == 2:
+        return pos[1] < lane.eip[1]
+
+    # Bottom lanes
+    elif l_index == 3:
+        return pos[0] < lane.eip[0]
+    elif l_index == 4:
+        return pos[1] > lane.eip[1]
+    elif l_index == 5:
+        return pos[0] > lane.eip[0]
+
+    # Right lanes
+    elif l_index == 6:
+        return pos[1] < lane.eip[1]
+    elif l_index == 7:
+        return pos[0] < lane.eip[0]
+    elif l_index == 8:
+        return pos[1] > lane.eip[1]
+
+    # Top lanes
+    elif l_index == 9:
+        return pos[0] > lane.eip[0]
+    elif l_index == 10:
+        return pos[1] < lane.eip[1]
+    elif l_index == 11:
+        return pos[0] < lane.eip[0]
+
+
+# Given a car, this function will calculate and return
+# the new position  while in the intersection by utilizing
+# h is the x position for the center of the circle,
+# k is the y position for the center of the circle, and
+# r is the radius
+# angle is the angle of the turn
+def intersection_pos(car):
+    lane = car.lane
+    x = car.position[0]
+    y = car.position[1]
+    
+    if lane.index == 0:
+        h = lane.sip[0]
+        k = lane.eip[1]
+        r = abs(h - k)
+        angle = math.radians(-45)
+        omega = .01 + car.vel * .0005
+
+        angle = angle + omega
+        x = x + r * omega * math.cos(angle + math.pi / 2)
+        y = y + r * omega * math.sin(angle + math.pi / 2)
+        return [x,y]
+        
+    elif lane.index == 1:
+        return [x + car.vel, y]
+
+    elif lane.index == 2:
+        h = lane.sip[0]
+        k = lane.eip[1]
+        r = abs(h - k)
+        angle = math.radians(70)
+        omega = .01 + car.vel * .003
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y - r * omega * math.sin(angle + math.pi / 2)
+        return [x,y]
+        
+    elif lane.index == 3:
+        h = lane.sip[1]
+        k = lane.eip[0]
+        r = abs(h - k)
+        angle = math.radians(-57)
+        omega = .01 + car.vel * .025
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y + r * omega * math.sin(angle + math.pi / 2)
+        #print [x,y]
+        return [x,y]
+
+    elif lane.index == 4:
+        return [x, y + car.vel]
+
+    elif lane.index == 5:
+        h = lane.sip[0]
+        k = lane.eip[1]
+        r = abs(h - k)
+        angle = math.radians(60)
+        omega = .01 + car.vel * .0005
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y + r * omega * math.sin(angle + math.pi / 2)
+        #print [x, y]
+        return [x,y]
+
+    elif lane.index == 6:
+        h = lane.sip[0]
+        k = lane.eip[1]
+        r = abs(h - k)
+        angle = math.radians(-50)
+        omega = .01 + car.vel * .0005
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y - r * omega * math.sin(angle + math.pi / 2)
+        #print [x, y]
+        return [x,y]
+    
+    elif lane.index == 7:
+        return [x - car.vel, y]
+    
+    elif lane.index == 8:
+        h = lane.sip[0]
+        k = lane.eip[1]
+        r = abs(h - k)
+        angle = math.radians(-40)
+        omega = .01 + car.vel * .03
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y + r * omega * math.sin(angle + math.pi / 2)
+        #print [x, y]
+        return [x,y]
+
+    elif lane.index == 9:
+        h = lane.sip[1]
+        k = lane.eip[0]
+        r = abs(h - k)
+        angle = math.radians(-45)
+        omega = .01 + car.vel * .02
+
+        angle = angle + omega
+        x = x + r * omega * math.cos(angle + math.pi / 2)
+        y = y - r * omega * math.sin(angle + math.pi / 2)
+        #print [x, y]
+        return [x,y]
+
+    elif lane.index == 10:
+        return [x, y - car.vel]
+
+    elif lane.index == 11:
+        h = lane.sip[1]
+        k = lane.eip[0]
+        r = abs(h - k)
+        angle = math.radians(-35)
+        omega = .01 + car.vel * .0005
+
+        angle = angle + omega
+        x = x - r * omega * math.cos(angle + math.pi / 2)
+        y = y - r * omega * math.sin(angle + math.pi / 2)
+        print [x, y]
+        return [x,y]
--- simulation.py
+++ simulation.py
@@ -15,26 +15,31 @@ class SimWindow(pyglet.window.Window):
     MAX_ACC = 1 # Maximum acceleration
     MAX_DEC = -1 # Maximum decceleration
     SPAWN_RATE = 20 # A new car will come approximately 1/SPAWN_RATE ticks
+
+    # Car height
+    CH = 30
+    # Car width
+    CW = 42
     
     # Initialize lanes
     # starting car position, intersection start,
     # intersection end, ending car position
     # Need to check later to make sure ending inter and car positions are okay
-    lane_0 = lane([0,430], [440, 430], [848,828], [848, 1044], 0) # pink left
-    lane_1 = lane([0,360], [440, 360], [1060, 360], [1502, 360], 1) # pink straight
-    lane_2 = lane([0,295], [440, 295], [473, 234], [473, 0], 2) # pink right
+    lane_0 = lane([0, 482-CH], [300-CW, 482-CH], [518, 700+CH], [518, 1000+CH], 0) # pink left
+    lane_1 = lane([0, 415-CH], [300-CW, 415-CH], [700, 415-CH], [1000, 415-CH], 1) # pink straight
+    lane_2 = lane([0, 348-CH], [300-CW, 348-CH], [318, 300], [318, 0], 2) # pink right
 
-    lane_3 = lane([855,0], [855, 242], [428, 630], [0, 630], 3) # green left
-    lane_4 = lane([930,0], [930, 242], [930, 808], [930, 1044], 4) # green straight
-    lane_5 = lane([987,0], [987, 242], [1064, 323], [1502, 323], 5) # green right
+    lane_3 = lane([518, 0], [518, 300], [300-CW, 548-CH], [0, 548-CH], 3) # green left
+    lane_4 = lane([585, 0], [585, 300], [585, 700+CW], [585, 1000+CW], 4) # green straight
+    lane_5 = lane([652, 0], [652, 300], [700, 348], [1000, 348], 5) # green right
 
-    lane_6 = lane([1502,626], [1055, 626], [628, 250], [628, 0], 6) # yellow left
-    lane_7 = lane([1502,690], [1055, 690], [448, 690], [0, 690], 7) # yellow straight
-    lane_8 = lane([1502,758], [1055, 758], [1005, 789], [1005, 1044], 8) # yellow right
+    lane_6 = lane([1000, 548-CH], [700, 548-CH], [452, 300], [452, 0], 6) # yellow left
+    lane_7 = lane([1000, 615-CH], [700, 615-CH], [300-CW, 615-CH], [0, 615-CH], 7) # yellow straight
+    lane_8 = lane([1000, 682-CH], [700, 682-CH], [652, 700+CH], [652, 1000+CW], 8) # yellow right
 
-    lane_9 = lane([640,1044], [640, 789], [1055, 439], [1502, 439], 9) # blue left
-    lane_10 = lane([575,1044], [575, 789], [575, 250], [575, 0], 10) # blue straight
-    lane_11 =  lane([508,1044], [508, 789], [448, 737], [0, 737], 11) # blue right
+    lane_9 = lane([452, 1000+CW], [452, 700+CW], [700, 485], [1000, 485], 9) # blue left
+    lane_10 = lane([385, 1000+CW], [385, 700+CW], [385, 300], [385, 0], 10) # blue straight
+    lane_11 =  lane([318, 1000+CW], [318, 700+CW], [300-CW, 682-CH], [0, 682-CH], 11) # blue right
     
     # --------------- END CONSTANT DEFINITONS ----------------
     
@@ -43,7 +48,7 @@ class SimWindow(pyglet.window.Window):
         self.set_location(200, 200) # Set position of window on screen
         self.frame_rate = 1/30.0
 
-        self.background = SimObject(0, 0, 'resources/road.png')
+        self.background = SimObject(0, 0, 'resources/intersection.png')
         self.loc = []
 
     # Drawing stuff onto the scene
@@ -54,7 +59,7 @@ class SimWindow(pyglet.window.Window):
         # Draw all cars in loc
         for c in self.loc:
             temp = SimObject(c.position[0], c.position[1], 'resources/'+c.color+'_car.png')
-            temp.sprite.rotation = degrees(atan2(c.vel[1], c.vel[0]))
+            temp.sprite.rotation = c.rotation
             temp.sprite.draw()
 
             
@@ -64,40 +69,40 @@ class SimWindow(pyglet.window.Window):
         for c in self.loc:
             # GAME THEORY STUFF DONE HERE, CHANGE ACCELERATIONS
             # Update car's acceleration each tick
-            c.on_tick([0,0])
+            c.on_tick(0)
 
         # Randomly generate a new car - TODO: MAKE SURE CARS DONT SPAWN
         # ON TOP OF EACH OTHER
         if(randint(0,self.SPAWN_RATE) == 0):
             lane = randint(0, 11)
             if lane == 0:
-                self.loc.append(car(deepcopy(self.lane_0), [0,0], 'blue'))
+                self.loc.append(car(deepcopy(self.lane_0), 'blue'))
             if lane == 1:
-                self.loc.append(car(deepcopy(self.lane_1), [0,0], 'yellow'))
+                self.loc.append(car(deepcopy(self.lane_1), 'yellow'))
             if lane == 2:
-                self.loc.append(car(deepcopy(self.lane_2), [0,0], 'green'))
+                self.loc.append(car(deepcopy(self.lane_2), 'green'))
             if lane == 3:
-                self.loc.append(car(deepcopy(self.lane_3), [0,0], 'pink'))
+                self.loc.append(car(deepcopy(self.lane_3), 'pink'))
             if lane == 4:
-                self.loc.append(car(deepcopy(self.lane_4), [0,0], 'blue'))
+                self.loc.append(car(deepcopy(self.lane_4), 'blue'))
             if lane == 5:
-                self.loc.append(car(deepcopy(self.lane_5), [0,0], 'yellow'))
+                self.loc.append(car(deepcopy(self.lane_5), 'yellow'))
             if lane == 6:
-                self.loc.append(car(deepcopy(self.lane_6), [0,0], 'green'))
+                self.loc.append(car(deepcopy(self.lane_6), 'green'))
             if lane == 7:
-                self.loc.append(car(deepcopy(self.lane_7), [0,0], 'pink'))
+                self.loc.append(car(deepcopy(self.lane_7), 'pink'))
             if lane == 8:
-                self.loc.append(car(deepcopy(self.lane_8), [0,0], 'blue'))
+                self.loc.append(car(deepcopy(self.lane_8), 'blue'))
             if lane == 9:
-                self.loc.append(car(deepcopy(self.lane_9), [0,0], 'yellow'))
+                self.loc.append(car(deepcopy(self.lane_9), 'yellow'))
             if lane == 10:
-                self.loc.append(car(deepcopy(self.lane_10), [0,0], 'green'))
+                self.loc.append(car(deepcopy(self.lane_10), 'green'))
             if lane == 11:
-                self.loc.append(car(deepcopy(self.lane_11), [0,0], 'pink'))
+                self.loc.append(car(deepcopy(self.lane_11), 'pink'))
 
             
 if __name__ == "__main__":
-        window = SimWindow(1502, 1044, "Test", resizable=False)
+        window = SimWindow(1000, 1000, "Test", resizable=False)
         pyglet.clock.schedule_interval(window.update, window.frame_rate)
         pyglet.app.run()
         
